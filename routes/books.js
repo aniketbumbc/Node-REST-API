eed6566c44557762b478de190ba1e5f66cbbd765
Home Home <aniket.umbc@gmail.com>
2020-05-11 19:04:15 +0530

    update functionality

--- routes/books.js
+++ routes/books.js
@@ -19,3 +19,15 @@ exports.findSingleBook = async (req, res) => {
     res.status(404).send({ error: "Book is not found" });
   }
 };
+
+exports.updateBook = async (req,res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        Object.assign(book,req.body);
+        book.save();
+        res.send({data:book});
+      } catch {
+        res.status(404).send({ error: "Book is not found" });
+      }
+
+}
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ mongoose
     app.get("/books", bookRoute.findBooks);
     app.post("/books",bookRoute.createBook);
     app.get("/books/:id",bookRoute.findSingleBook);
+    app.patch("/books/:id",bookRoute.updateBook);
 
     app.listen(8000, () => {
       console.log("Server started port 8000");
